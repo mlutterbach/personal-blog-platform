e13734ec465ff5f51e53614660b9acf6ac525f5c
Marcos Lutterbach <mlutterb@gmail.com>
2024-11-02 23:28:01 +0100

    Added Contact to send email from react form

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   root "articles#index"
 
   resources :articles
+
+  namespace :api do
+    namespace :v1 do
+      resources :contacts, only: [:create]
+    end
+  end
 end
