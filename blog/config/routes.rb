2f91064856410e0351c1c1ba34c97424dfff0ad7
Marcos Lutterbach <mlutterb@gmail.com>
2024-11-05 00:17:03 +0100

    Added users table for registration and login

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   root "articles#index"
 
   resources :articles
+  get '/users', to: 'users#index'
+  post '/signup', to: 'users#create'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   namespace :api do
     namespace :v1 do
