4d38e1a767245544d494953c826dc62ddebb8b85
Marcos Lutterbach <mlutterb@gmail.com>
2024-11-06 18:59:13 +0100

    Added active storage, changed articles to have screenshots attached

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,19 +1,19 @@
 class ArticlesController < ApplicationController
+  include ActiveStorage::SetCurrent
+  before_action :authenticate_user!, only: [:create, :edit, :update, :destroy]
   before_action :set_article, only: %w[show edit update destroy]
+
   # GET /articles
   def index
-    @articles = Article.all
-    @articles = Article.where('tags LIKE ?', "%#{params[:tags]}%") if params[:tags]
+    @articles = Article.order(created_at: :desc)
+    @articles = @articles.where('tags LIKE ?', "%#{params[:tags]}%") if params[:tags]
 
-    respond_to do |format|
-      format.html
-      format.json { render json: @articles }
-    end
+    render json: @articles.as_json(include: { screenshots: { methods: :url } })
   end
 
   # GET /articles/:id
   def show
-    render json: @article
+    render json: @article.as_json(include: { screenshots: { methods: :url } })
   end
 
   # GET /articles/new
@@ -26,7 +26,7 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
 
     if @article.save
-      render json: @article, status: :created
+      render json: @article.as_json(include: { screenshots: { methods: :url } }), status: :created
     else
       render json: @article.errors, status: :unprocessable_entity
     end
@@ -39,13 +39,13 @@ class ArticlesController < ApplicationController
   # PATCH/PUT /articles/:id
   def update
     if @article.update(article_params)
-      render json: @article
+      render json: @article.as_json(include: { screenshots: { methods: :url } })
     else
       render json: @article.errors, status: :unprocessable_entity
     end
   end
 
-  # Delete /articles/:id
+  # DELETE /articles/:id
   def destroy
     @article.destroy
     head :no_content
@@ -54,10 +54,10 @@ class ArticlesController < ApplicationController
   private
 
   def set_article
-    @article = Article.find(params[:id])
+    @article = Article.includes(screenshots_attachments: :blob).find(params[:id])
   end
 
   def article_params
-    params.require(:article).permit(:title, :content, :tags)
+    params.require(:article).permit(:title, :content, :tags, screenshots: [])
   end
 end
