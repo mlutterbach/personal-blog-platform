ffd20b8f6e1dccf9a68dfe823b335d8d5fd2535a
Marcos Lutterbach <mlutterb@gmail.com>
2024-11-09 11:14:00 +0100

    Changed to Postgres db

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module Blog
     # config.eager_load_paths << Rails.root.join("extras")
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins '*'
+        origins 'localhost:3000'
         resource '*',
           headers: :any,
           methods: [:get, :post, :put, :patch, :delete, :options, :head]
