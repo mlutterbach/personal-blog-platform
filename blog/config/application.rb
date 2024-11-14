5223eabb64a14db68089e3c02bfbc2eebb6fca21
Marcos Lutterbach <mlutterb@gmail.com>
2024-11-14 22:49:22 +0100

    CORS website adjustment

--- blog/config/application.rb
+++ blog/config/application.rb
@@ -25,7 +25,7 @@ module Blog
     # config.eager_load_paths << Rails.root.join("extras")
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'localhost:3001', 'localhost:3000'
+        origins 'localhost:3001', 'localhost:3000', 'https://personal-blog-platform.onrender.com'
         resource '*',
           headers: :any,
           methods: [:get, :post, :put, :patch, :delete, :options, :head]
